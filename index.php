c87fc8230896b36c87831114ea7e8c80d6716b1e
kodekito <kodekito@gmail.com>
2020-11-13 19:35:31 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Project ABC</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-CuOF+2SnTUfTwSZjCXf01h7uYhfOBuxIhGKPbfEJ3+FqH/s6cIFN9bGr1HmAg4fQ" crossorigin="anonymous">
+</head>
+<body>
+    <?php
+      require "layouts/header.php";
+      $page = isset($_GET['page']) ? $_GET['page'] : 'home';
+    ?>
+    <main class="container">
+      <?php
+        require $page . '.php';
+      ?>
+    </main>
+    <?php
+      require "layouts/footer.php";
+    ?>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-popRpmFF9JQgExhfw5tZT4I9/CI5e2QcuUZPOVXb1m7qUmeR2b50u+YFEYe1wgzy" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ layouts/footer.php
@@ -0,0 +1,3 @@
+<footer class="mt-auto fixed-bottom" style='text-align: center'>
+    <p>Created By:<a href="#"> Onesinus SPT</a>.</p>
+</footer>
